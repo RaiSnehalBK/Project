4dbef5c1c271e2a8ad95eea2e8592f969fe57bc3
Rai Snehal B <88423472+RaiSnehalBK@users.noreply.github.com>
2025-09-02 09:54:49 +0530

    Update urls.py

--- mywebapp/main/urls.py
+++ mywebapp/main/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
-    path('location/', views.location, name='location'),
     path('profile/', views.profile, name='profile'),
     path('settings/', views.settings, name='settings'),    
     path('logout/', views.logout, name='logout'),    
     path("video_feed/", views.video_feed, name="video_feed"),
-    path("profile/update/", views.update_profile, name="update_profile"),
-    path("profile/upload-photo/", views.upload_photo, name="upload_photo"),
     
 ]
